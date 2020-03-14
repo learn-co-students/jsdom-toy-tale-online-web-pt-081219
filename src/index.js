2276457ffe2acb3e8b2cc700f128d56fd8f7303b
JCGuest <jcguest90@gmail.com>
2020-03-14 02:11:01 -0500

    toy img, h2 and like count/button

--- src/index.js
+++ src/index.js
@@ -12,4 +12,56 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
+
+  let toyCollection = document.getElementById('toy-collection')
+  fetch('http://localhost:3000/toys')
+  .then(function(response) {
+    return response.json();
+})
+  .then(function(json) {
+    let keys = []
+    keys.push(Object.keys(json))
+    for (const element of keys['0']) {
+      addToy(json[element])
+    }
+
 });
+
+function addToy(toy) {
+  //create card
+  let collection = document.querySelector("#toy-collection")
+  let card = document.createElement('div')
+  card.id = `${toy.name}-card`
+  //name
+  let name = document.createElement('h2')
+  name.innerHTML = toy.name
+  card.appendChild(name)
+  // likes count
+  let likes = document.createElement('p')
+  likes.innerHTML = `${toy.name} has ${toy.likes} likes`
+  card.appendChild(likes)
+  // like button
+  let likeBtn = document.createElement('button')
+  likeBtn.id = toy.id
+  likeBtn.innerHTML = "Like"
+  card.appendChild(likeBtn)
+  likeBtn.addEventListener('click', (e) => {
+    likeToy(e)
+  })
+  //img
+  let image = document.createElement('img')
+  image.src = toy.image
+  card.appendChild(image)
+
+  collection.appendChild(card)
+};
+
+function likeToy(e) {
+  console.log(e.target)
+}
+});
+
+// h2 tag with the toy's name
+// img tag with the src of the toy's image attribute and the class name "toy-avatar"
+// p tag with how many likes that toy has
+// button tag with a class "like-btn"
