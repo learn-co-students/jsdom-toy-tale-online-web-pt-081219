99c147a279dd37a90e1ef17ee5287fa9a9f0e983
saramccombs <theSaraMcCombs@gmail.com>
2020-03-22 17:19:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -9,10 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
   addBtn.addEventListener("click", () => {
     // When you click the 'add new toy' button
     // addToy variable switches to the opposite value
-    // which renders and hides the add toy form
+    // which renders or hides the add toy form
     addToy = !addToy;
     if (addToy) {
       toyForm.style.display = "block";
+      // Adds event listener for the submit button
+      toyForm.addEventListener('submit', event => {
+        event.preventDefault()
+        // I don't understand the event.target part of the next line
+        // What is event.target? What am I passing to this function?
+        postNewToy(event.target)
+      })
     } else {
       toyForm.style.display = "none";
     }
@@ -21,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // Function to get the toys via fetch get request
   function getToys() {
     return fetch('http://localhost:3000/toys')
-    .then(res => res.json())
+    .then(response => response.json())
     // sends the each toy object to the createToyCards
     // function to be rendered in the DOM
     .then(toys => toys.forEach(toy => createToyCards(toy)))
@@ -46,6 +53,11 @@ document.addEventListener("DOMContentLoaded", () => {
     toyBtn.innerText = 'Like <3'
     toyBtn.setAttribute('class', 'like-btn')
 
+    // Add event listener for incrementing likes
+    toyBtn.addEventListener('click', (event) => {
+      likes(event)
+    })
+
     // Append all the new elements to the new div
     toyCardDiv.appendChild(toyH2)
     toyCardDiv.appendChild(toyImg)
@@ -53,6 +65,48 @@ document.addEventListener("DOMContentLoaded", () => {
     toyCardDiv.appendChild(toyBtn)
     // Append the new div to the existing div
     toyCollectionDiv.appendChild(toyCardDiv)
+  }
+
+  function postNewToy(toyData) {
+    // Makes post request with form data
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        "name": toyData.name.value,
+        "image": toyData.image.value,
+        "likes": 0
+
+      })
+    })
+    .then(response => response.json())
+    .then((toy) => {
+      createToyCards(toy)
+    })
+  }
+
+  function likes(event) {
+    event.preventDefault()
+    let incrementedLike = parseInt(event.target.previousElementSibling.innerText) + 1
+
+    fetch(`http://localhost:3000/toys/${event.target.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+
+      },
+      body: JSON.stringify({
+        "likes": incrementedLike
+      })
+    })
+    .then(response => response.json())
+    .then((toy => {
+      event.target.previousElementSibling.innerText = `${incrementedLike} likes`
+    }))
   }
 
   getToys()
