08c7f16466718efd97f4b2977f9b2923ea11ba5e
saramccombs <theSaraMcCombs@gmail.com>
2020-03-22 14:04:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +14,34 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
+
+  function getToys() {
+    return fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toys => toys.forEach(toy => createToyCards(toy)))
+  }
+
+  function createToyCards(toy) {
+    const toyCollectionDiv = document.querySelector('#toy-collection')
+    const toyCardDiv = document.createElement('div')
+    const toyH2 = document.createElement('h2')
+    const toyImg = document.createElement('img')
+    const toyP = document.createElement('p')
+    const toyBtn = document.createElement('button')
+
+    toyH2.innerText = toy.name
+    toyImg.src = toy.image
+    toyImg.width = '100'
+    toyImg.class = 'toy-avatar'
+    toyP.innerText = `${toy.likes} likes`
+    toyBtn.innerText = 'Like <3'
+
+    toyCardDiv.appendChild(toyH2)
+    toyCardDiv.appendChild(toyImg)
+    toyCardDiv.appendChild(toyP)
+    toyCardDiv.appendChild(toyBtn)
+    toyCollectionDiv.appendChild(toyCardDiv)
+  }
+
+  getToys()
 });
